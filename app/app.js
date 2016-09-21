0293e39b3a26875eb2bb41bc03bfbf245e8ee14f
lucasxrf <lucasxrf@figueiredo>
2016-09-20 21:59:38 -0300

    Melhoria na apresentação do daily Início da construção dos atributos de scope do daily

--- app/app.js
+++ app/app.js
@@ -2,7 +2,21 @@
 
 angular.module('f.scheduler', [
     'ui.bootstrap'
-]).run(function() {
+]).run(function ($rootScope) {
     moment.locale('pt-BR');
+    $rootScope.appoint = [
+        {
+            hour: '08:15',
+            description: 'Cagar'
+        },
+        {
+            hour: '10:15',
+            description: 'Escovar dente'
+        },
+        {
+            hour: '12:20',
+            tretas: 'Lokas'
+        }];
+    
 });
 
--- app/modules/daily/daily.directive.js
+++ app/modules/daily/daily.directive.js
@@ -1,16 +1,15 @@
-angular.module('f.scheduler').directive('fKingDaily', [
-    'CalendarService',
-    function (CalendarService) {
-        return {
-            templateUrl: 'modules/daily/daily.template.html',
-            restrict: 'E',
-            replace: true,
-            scope: {},
-            link: function (scope) {
-                scope.day = moment();
-                scope.dailyShown = false;
-                scope.day.appointments = ['08:15', '10:15', '12:20', '14:45', '16:20', '18:45'];
-            }
-        };
-    }
-]);
+angular.module('f.scheduler').directive('fKingDaily', function () {
+    return {
+        templateUrl: 'modules/daily/daily.template.html',
+        restrict: 'E',
+        replace: true,
+        scope: {
+            appointments: '='
+        },
+        link: function (scope) {
+            scope.day = moment();
+            scope.dailyShown = false;
+            scope.day.appointments = scope.appointments;
+        }
+    };
+});
